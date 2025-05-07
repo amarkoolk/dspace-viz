d4459d5b8b5d5fc95e1b4f32e22763666a8d3ddd
Srikar Gouru <srikarg89@gmail.com>
2025-05-06 22:56:53 -0400

    Update laguna origin

---
+++ track_meshes/gdi.py
@@ -0,0 +1,49 @@
+from stl import mesh
+import pygeodesy
+import numpy as np
+
+# Load the STL file
+stl_mesh = mesh.Mesh.from_file('dspace-laguna_track.stl')
+
+# Access mesh data (vertices, normals)
+vertices = stl_mesh.vectors
+normals = stl_mesh.normals
+
+david_frame = pygeodesy.ltp.LocalCartesian(
+    36.584015, # lat
+    -121.752034, # lon
+    754.0 # alt
+)
+
+# 36.58736160181 -121.75581850179
+dspace_frame = pygeodesy.ltp.LocalCartesian(
+    36.58736160181, # lat
+    -121.75581850179, # lon
+    754.0 # alt
+)
+
+def tolla(converter, arr):
+    _x, _y, _z, lat, lon, h, *_ = converter.reverse(arr[0], arr[1], arr[2])
+    return [lat, lon, h]
+
+def toxyz(converter, arr):
+    x, y, z, *_ = converter.forward(arr[0], arr[1], arr[2])
+    return [x, y, z]
+
+track = mesh.Mesh(np.zeros(vertices.shape[0], dtype=mesh.Mesh.dtype))
+
+new_vertices = np.zeros_like(vertices)
+for i, vector in enumerate(vertices):
+    v0_lla = tolla(david_frame, vector[0])
+    v1_lla = tolla(david_frame, vector[1])
+    v2_lla = tolla(david_frame, vector[2])
+
+    v0_xyz = toxyz(dspace_frame, v0_lla)
+    v1_xyz = toxyz(dspace_frame, v1_lla)
+    v2_xyz = toxyz(dspace_frame, v2_lla)
+
+    track.vectors[i][0] = v0_xyz
+    track.vectors[i][1] = v1_xyz
+    track.vectors[i][2] = v2_xyz
+
+track.save(f"laguna_updated_track.stl")
